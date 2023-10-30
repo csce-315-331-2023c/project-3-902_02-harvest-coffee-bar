e604a266d4449a3f3e924255a8d70c3df11b93a4
TomSterQaz <exln.tomsterqaz@gmail.com>
2023-10-30 17:07:33 -0500

    implemented front end test of database interaction

--- pages/manager.js
+++ pages/manager.js
@@ -1,16 +1,41 @@
-import connection from '../backend/database'
+// pages/manager.js
 
-export const getInventory = async () => {
-    const client = await connection.client()
-    const result = await client.query('SELECT * FROM orders')
-    client.release()
+import { useState } from 'react';
 
-    console.log(result)
+function manager() {
+    const [menuItems, setMenuItems] = useState([]);
+
+    const fetchMenuItems = async () => {
+        try {
+
+            //attempt to fetch menu items
+            const response = await fetch('api/get_menu');
+
+            if (response.ok) {
+                const data = await response.json();
+                setMenuItems(data);
+            } else {
+                console.error("Unable to fetch menu items.");
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Menu Items</h1>
+            <button onClick={fetchMenuItems}>Fetch Menu Items</button>
+            <ul>
+                {menuItems.map((menuItems) => (
+                    <li key={menuItems.menu_item_id}>
+                        Item ID: {menuItems.menu_item_id}, Item Name: {menuItems.menu_item_name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-const Manager = () => (
-    getInventory()
-    //<div>Howdy! This page is under construction</div>
-)
 
-export default Manager;
+export default manager;
