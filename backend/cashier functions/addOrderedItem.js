0a81f6210ae513616dda300f02671039781c8120
Matthew S McDonough <matthewsmcd2002@tamu.edu>
2023-10-30 18:33:55 -0500

    convert cashier functions to js

---
+++ backend/cashier functions/addOrderedItem.js
@@ -0,0 +1,29 @@
+import { Pool } from 'pg';
+
+async function addOrderedItem(connection, order_id, menu_item_id) {
+    const insertOrderedItem = `
+        INSERT INTO
+            ordered_items(ordered_id, menu_item_id, num_items)
+        VALUES
+            ($1, $2, 1)
+    `;
+
+    try {
+        const preppedCommand = await connection.prepare(insertOrderedItem);
+        
+        // Add values to the command
+        await preppedCommand.setInt(1, order_id);
+        await preppedCommand.setInt(2, menu_item_id);
+
+        // Execute the command
+        await preppedCommand.execute();
+
+        // Assuming you have an updateIngredients function, call it here
+        await updateIngredients(connection, menu_item_id);
+
+    } catch (err) {
+        console.log("An error has occurred.");
+        console.log("See full details below.");
+        console.error(err);
+    }
+}
