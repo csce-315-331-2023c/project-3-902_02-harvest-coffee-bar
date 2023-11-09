d00fb42e5e1f1b8238d51967bab8c704a486f74a
Matthew S McDonough <matthewsmcd2002@tamu.edu>
2023-11-08 18:14:15 -0600

    Updated add menu item function to the new format

--- pages/api/manager/add_item_to_menu.js
+++ pages/api/manager/add_item_to_menu.js
@@ -1,17 +1,38 @@
-// import connection from '../../../backend/database'
+import connection from '../../../backend/database'
 
-// export default async (req, res) => {
+export default async (req, res) => {
 
-//     try {
+    if (req.method != 'POST') {
+        res.status(405).send({ message: 'Invalid request: Only POST reqs allowed' });
+    }
 
-//         const statement = `
-//         INSERT INTO
-//             menu_items(menu_item_id, menu_item_name, menu_item_category, item_description, price)
-//         SELECT
-//             MAX(menu_item_id) + 1, $1, $2, $3, $4
-//         FROM
-//             menu_items
-//         `
+    const client = await connection.connect();
 
-//     }
-// }
+    try {
+
+        await client.query('BEGIN;');
+
+        const addMenuItemQuery = `
+        INSERT INTO
+            menu_items(menu_item_id, menu_item_name, menu_item_category, item_description, price)
+        SELECT
+            MAX(menu_item_id) + 1, $1, $2, $3, $4
+        FROM
+            menu_items;
+        `;
+
+        const itemParameters = [req.menu_item_name, req.menu_item_category, req.item_description, req.item_price];
+
+        client.query(addMenuItemQuery, itemParameters);
+
+    } catch (error) {
+
+        await client.query('ROLLBACK;');
+        throw (error);
+
+    } finally {
+
+        client.release();
+
+    }
+}
