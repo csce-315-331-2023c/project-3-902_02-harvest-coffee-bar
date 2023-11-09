ac8418c2c1b40aa6cebdb0d72151ded6b6d87864
Matthew S McDonough <matthewsmcd2002@tamu.edu>
2023-11-08 18:23:57 -0600

    add the commit to database message

--- pages/api/manager/add_item_to_menu.js
+++ pages/api/manager/add_item_to_menu.js
@@ -2,12 +2,15 @@ import connection from '../../../backend/database'
 
 export default async (req, res) => {
 
+    // check if POST req
     if (req.method != 'POST') {
         res.status(405).send({ message: 'Invalid request: Only POST reqs allowed' });
     }
 
+    // avoid race conditions
     const client = await connection.connect();
 
+    // add item to menu
     try {
 
         await client.query('BEGIN;');
@@ -25,13 +28,19 @@ export default async (req, res) => {
 
         client.query(addMenuItemQuery, itemParameters);
 
+        // push statements to database
+        await client.query('COMMIT;');
+        res.status(200).json({ message: "done" });
+
     } catch (error) {
 
+        // disregard if an error is caught
         await client.query('ROLLBACK;');
         throw (error);
 
     } finally {
 
+        // release client back into the pool
         client.release();
 
     }
