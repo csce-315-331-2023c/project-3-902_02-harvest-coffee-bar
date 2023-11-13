992b0020d82f914f711dff5a404ade2e25bda6c1
Matthew S McDonough <matthewsmcd2002@tamu.edu>
2023-11-13 08:50:53 -0600

    Add parameters to already made functions

--- pages/manager.js
+++ pages/manager.js
@@ -24,49 +24,45 @@ function manager() {
         }
     };
 
-    const addItemToMenu = async () => {
+    const addItemToMenu = async (menu_item_name, menu_item_category, item_description, price) => {
     
-        // TODO: ADD PARAMETERS FROM INPUT
         var payload = {
-            menu_item_name: "",
-            menu_item_category: "",
-            item_description: "",
-            price: 0.0
+            menu_item_name: menu_item_name,
+            menu_item_category: menu_item_category,
+            item_description: item_description,
+            price: price
         }
 
         await fetch(`${server}/api/manager/add_item_to_menu`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
     }
 
-    const deleteItemFromMenu = async () => {
+    const deleteItemFromMenu = async (menu_item_id) => {
 
-        // TODO: ADD PARAMETERS FROM INPUT
         var payload = {
-            menu_item_id: -1
+            menu_item_id: menu_item_id
         }
 
         await fetch(`${server}/api/manager/delete_item_from_menu`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
 
     }
 
-    const editMenuItem = async () => {
+    const editMenuItem = async (price, menu_item_id) => {
 
-        // TODO: ADD PARAMETERS FROM INPUT
         var payload = {
-            price: 0.0,
-            menu_item_id: -1
+            price: price,
+            menu_item_id: menu_item_id
         }
 
         await fetch(`${server}/api/manager/edit_menu_item`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
 
     }
 
-    const addIngredientsToMenuItem = async () => {
+    const addIngredientsToMenuItem = async (menu_item_id, ingredient_id, num_ingredients) => {
 
-        // TODO: ADD PARAMETERS FROM INPUT
         var payload = {
-            menu_item_id: -1, 
-            ingredient_id: -1, 
-            num_ingredients: -1
+            menu_item_id: menu_item_id, 
+            ingredient_id: ingredient_id, 
+            num_ingredients: num_ingredients
         }
 
         await fetch(`${server}/api/manager/add_ingredients_in_menu_item`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
@@ -79,41 +75,53 @@ function manager() {
 
     }
 
-    const updateItemInInventory = async () => {
+    const updateItemInInventory = async (ingredient_count, ingredient_id) => {
 
-        // TODO: ADD PARAMETERS FROM INPUT
         var payload = {
-            ingredient_count: 0, 
-            ingredient_id: -1
+            ingredient_count: ingredient_count, 
+            ingredient_id: ingredient_id
         }
 
         await fetch(`${server}/api/manager/update_inventory_item`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
 
     }
 
-    const addInventoryItem = async () => {
+    const addInventoryItem = async (ingredient_name, ingredient_count, max_ingredient_count) => {
 
-        // TODO: ADD PARAMETERS FROM INPUT
         var payload = {
-            ingredient_name: "",
-            ingredient_count: 0,
-            max_ingredient_count: 0
+            ingredient_name: ingredient_name,
+            ingredient_count: ingredient_count,
+            max_ingredient_count: max_ingredient_count
         }
 
         await fetch(`${server}/api/manager/add_inventory_item`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
 
     }
 
-    const deleteInventoryItem = async () => {
+    const deleteInventoryItem = async (ingredient_id) => {
 
-        // TODO: ADD PARAMETERS FROM INPUT
         var payload = {
-            ingredient_id: -1
+            ingredient_id: ingredient_id
         }
 
         await fetch(`${server}/api/manager/delete_inventory_item`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
 
     }
+
+    /* STATISTICAL FUNCTIONS */
+
+    const getSalesByTime = async (start_time, end_time, item_name) => {
+
+        var payload = {
+            startTime: start_time,
+            endTime: end_time,
+            itemName: item_name
+        }
+
+        await fetch(`${server}/api/manager/get_sales_by_time`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
+
+    }
+
 
     return (
         <div>
