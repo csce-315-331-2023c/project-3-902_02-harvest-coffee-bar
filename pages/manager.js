67ed5f6f672ab5c55e72e9c09ff96e75d62c647d
TianchengWu4810-tamu <tianchengwu4810@tamu.edu>
2023-11-14 04:44:54 -0600

    merge from matthew

--- pages/manager.js
+++ pages/manager.js
@@ -14,7 +14,7 @@ function Manager() {
     const [selectedItem, setSelectedItem] = useState('All');
     const [inventoryItems, setInventoryItems] = useState([]);
     const [excessReports, setExcessReports] = useState([]);
-    const [employeeSchedules, setEmployeeSchedules] = useState([]);
+    // const [employeeSchedules, setEmployeeSchedules] = useState([]);
 
     const [isMenuVisible, setIsMenuVisible] = useState(false); 
     const toggleMenuVisibility = () => {
@@ -23,7 +23,6 @@ function Manager() {
 
     const fetchMenuItems = async () => {
         try {
-
             //attempt to fetch menu items
             const response = await fetch(`${server}/api/manager/get_menu`);
 
@@ -223,186 +222,6 @@ function Manager() {
     };
     //Back-end implement needed
     const fetchEmployeeSchedules = () => { /* ... */ };
-
-
-    fetchMenuItems();
-    const addItemToMenu = async (menu_item_name, menu_item_category, item_description, price) => {
-    
-        var payload = {
-            menu_item_name: menu_item_name,
-            menu_item_category: menu_item_category,
-            item_description: item_description,
-            price: price
-        }
-
-        await fetch(`${server}/api/manager/add_item_to_menu`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
-
-    }
-
-    const deleteItemFromMenu = async (menu_item_id) => {
-
-        var payload = {
-            menu_item_id: menu_item_id
-        }
-
-        await fetch(`${server}/api/manager/delete_item_from_menu`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
-    }
-
-    const editMenuItem = async (price, menu_item_id) => {
-
-        var payload = {
-            price: price,
-            menu_item_id: menu_item_id
-        }
-
-        await fetch(`${server}/api/manager/edit_menu_item`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
-
-    }
-
-    const addIngredientsToMenuItem = async (menu_item_id, ingredient_id, num_ingredients) => {
-
-        var payload = {
-            menu_item_id: menu_item_id, 
-            ingredient_id: ingredient_id, 
-            num_ingredients: num_ingredients
-        }
-
-        await fetch(`${server}/api/manager/add_ingredients_in_menu_item`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
-
-    }
-
-    const viewAllInInventory = async () => {
-
-        try {
-            const response = await fetch(`${server}/api/manager/view_all_in_inventory`, { method: 'POST', headers: { 'Content-Type': 'application/json' }});
-        
-            if (response.ok) {
-                const data = await response.json();
-            } else {
-                console.error("Unable to view inventory.");
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-        
-    }
-
-    const updateItemInInventory = async (ingredient_count, ingredient_id) => {
-
-        var payload = {
-            ingredient_count: ingredient_count, 
-            ingredient_id: ingredient_id
-        }
-
-        await fetch(`${server}/api/manager/update_inventory_item`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
-
-    }
-
-    const addInventoryItem = async (ingredient_name, ingredient_count, max_ingredient_count) => {
-
-        var payload = {
-            ingredient_name: ingredient_name,
-            ingredient_count: ingredient_count,
-            max_ingredient_count: max_ingredient_count
-        }
-
-        await fetch(`${server}/api/manager/add_inventory_item`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
-
-    }
-
-    const deleteInventoryItem = async (ingredient_id) => {
-
-        var payload = {
-            ingredient_id: ingredient_id
-        }
-
-        await fetch(`${server}/api/manager/delete_inventory_item`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
-
-    }
-
-    /* STATISTICAL FUNCTIONS */
-
-    const getSalesByTime = async (start_time, end_time, item_name) => {
-
-        var payload = {
-            startTime: start_time,
-            endTime: end_time,
-            itemName: item_name
-        }
-
-        try {
-            const response = await fetch(`${server}/api/manager/get_sales_by_time`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) });
-        
-            if (response.ok) {
-                const report = await response.json();
-            } else {
-                console.error("Unable to fetch sales report.");
-            }
-        } catch (error) {
-            console.error("Error:", error);
-        }
-    }
-
-    const getExcessReport = async (start_date) => {
-
-        var payload = {
-            start_date: start_date
-        }
-        
-        try {
-            const response = await fetch(`${server}/api/manager/get_excess_report`, { method: 'POST', headers: { 'Content-Type': 'applications/json' }, body: JSON.stringify(payload) });
-            
-            if (response.ok) {
-                const report = await response.json();
-            } else {
-                console.error("Unable to fetch excess report.");
-            }
-
-        } catch (error) {
-            console.error("Error:", error);
-        }
-    }
-
-    const getLowStock = async () => {
-
-        try {
-            const response = await fetch(`${server}/api/manager/get_low_stock`, { method: 'POST', headers: { 'Content-Type': 'applications/json' } });
-            
-            if (response.ok) {
-                const report = await response.json();
-            } else {
-                console.error("Unable to fetch low stock items.");
-            }
-        } catch (error) {
-            console.error("Error:", error);
-        }
-    }
-
-    const getWhatSellsTogether = async (start_time, end_time) => {
-
-        var payload = {
-            start_time: start_time,
-            end_time: end_time
-        }
-
-        try {
-            const response = await fetch(`${server}/api/manager/get_what_sells_together`, { method: 'POST', headers: { 'Content-Type': 'applications/json' }, body: JSON.stringify(payload) });
-        
-            if (response.ok) {
-                const report = await response.json();
-            } else {
-                console.error("Unable to get paired item trend report.");
-            }
-        } catch (error) {
-            console.error("Error:", error);
-        }
-    }
-
-
-    //Back-end implement needed
-
-
-
     fetchMenuItems();
     return (
         <div className={managerStyles.ManagerGUI}>
@@ -475,7 +294,7 @@ function Manager() {
             {/* Inventory List Section */}
             <section>
                 <h2>Inventory List</h2>
-                <button onClick={fetchInventoryItems}>Fetch Inventory</button>
+                <button onClick={viewAllInInventory}>view All In Inventory</button>
                 <ul>
                     {inventoryItems.map((item, index) => (
                         <li key={index}>
