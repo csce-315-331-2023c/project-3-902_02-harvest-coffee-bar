9097d6907e9a4fc3f03826261ece6f58dd9330a1
niteshduraivel@tamu.edu <niteshduraivel@tamu.edu>
2023-11-13 18:18:33 -0600

    login_2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import navStyles from './components/NavBar.module.css';
 import coffeeStyles from './components/harvestCoffeeBar.module.css';
+import loginStyles from "./components/login.module.css";
 import Link from 'next/link';
 
 const Index = () => (
@@ -14,6 +15,10 @@ const Index = () => (
 		<div className={coffeeStyles.coffeeBar}>
 			<h1>Welcome to Our Coffee Bar</h1>
 			<p>Discover the finest coffee experience in town.</p>
+		</div>
+		<div className={loginStyles.loginContainer}> {/* Use the login styles here */}
+			<h1 className={loginStyles.loginTitle}>Please Login</h1>
+			<a href="/auth/google" className={loginStyles.googleButton}>Sign in with Google</a>
 		</div>
 	</div>
 )
