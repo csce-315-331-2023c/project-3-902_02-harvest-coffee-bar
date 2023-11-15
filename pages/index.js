ec5545d79caedcd52eda7b204252f5e277c885b0
TomSterQaz <exln.tomsterqaz@gmail.com>
2023-11-14 22:01:21 -0600

    fixed login

--- pages/index.js
+++ pages/index.js
@@ -64,11 +64,6 @@ const Index = () => {
 	return (
 		<div>
 			<NavBar />
-			<li className={`${navStyles.li} ${navStyles.login}`}>
-				<button onClick={() => signIn('google')} className={`${loginStyles.LoginLink} ${navStyles.button}`}>
-					<a className={loginStyles.LoginLink}>Login</a>
-				</button>
-			</li>
 			<div className={landingStyles.header}>
 				<div className={landingStyles.headerImage}></div>
 				<div className={landingStyles.headerText}>
