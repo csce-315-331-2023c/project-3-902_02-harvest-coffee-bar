52909bd32cf9ae43403deeb4f58a223c07feec30
niteshduraivel@tamu.edu <niteshduraivel@tamu.edu>
2023-12-05 12:36:36 -0600

    login email property undefined

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,7 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      scope: ['profile', 'email'],
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET, 
@@ -14,23 +15,29 @@ export default NextAuth({
   },
   callbacks: {
     async jwt(token, user) {
-      if (user) {
-        console.log('In here');
-        if (user.email == 'niteshduraivel@tamu.edu') {
+      console.log('Token:', token);
+      console.log('Got in here');
+      console.log('Token token email: ', token.user.email);
+
+      if (token.user.email) {
+        console.log('I got in token.email');
+
+        if (token.user.email === 'niteshduraivel@tamu.edu') {
           console.log('Admin login detected');
           token.role = 'admin';
         } 
         // else if (user.email.endsWith('@tamu.edu')) {
         //   token.role = 'manager';
         // } 
-        else if (user.email === 'cashier@example.com') {
+        else if (token.user.email === 'cashier@example.com') {
           token.role = 'cashier';
         } else {
           token.role = 'customer';
         }
 
-        token.id = user.id;
+        token.id = token.sub || '';
       }
+
       console.log('Out here');
       return token;
     },
