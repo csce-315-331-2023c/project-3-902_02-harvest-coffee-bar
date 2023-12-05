c3095e10d79e86d4663f8efe523145b436f14372
niteshduraivel@tamu.edu <niteshduraivel@tamu.edu>
2023-12-04 20:34:38 -0600

    user access login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
   providers: [
@@ -8,20 +8,33 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET, // Set your secret using an environment variable
+  secret: process.env.NEXTAUTH_SECRET, 
   session: {
     jwt: true,
   },
   callbacks: {
     async jwt(token, user) {
       if (user) {
-        token.id = user.id
+        if (user.email.endsWith('@tamu.edu')) {
+          token.role = 'admin';
+        } 
+        // else if (user.email.endsWith('@tamu.edu')) {
+        //   token.role = 'manager';
+        // } 
+        else if (user.email === 'cashier@example.com') {
+          token.role = 'cashier';
+        } else {
+          token.role = 'customer';
+        }
+
+        token.id = user.id;
       }
-      return token
+      return token;
     },
     async session(session, token) {
-      session.user.id = token.id
-      return session
+      session.user.id = token.id;
+      session.user.role = token.role;
+      return session;
     },
   },
-})
+});
