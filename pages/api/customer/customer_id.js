b4cf4496a7475645230397230d04b23f24f7c8eb
niteshduraivel@tamu.edu <niteshduraivel@tamu.edu>
2023-12-05 21:27:54 -0600

    finally

--- pages/api/customer/customer_id.js
+++ pages/api/customer/customer_id.js
@@ -16,7 +16,9 @@ export default async function handler(req, res) {
       customerId = rows[0].customer_id;
     } else {
       // If the customer doesn't exist, create a new one
-      const result = await connection.query('INSERT INTO customer(customer_name, customer_points, customer_email) VALUES($1, $2, $3) RETURNING customer_id', [name, points, email]);
+      const newIdQuery = await connection.query("SELECT MAX(customer_id) + 1 as new_id FROM orders;");
+      const newCustomerID = newIdQuery.rows[0].new_id;
+      const result = await connection.query('INSERT INTO customer(customer_id, customer_name, customer_points, customer_email) VALUES($1, $2, $3, $4)', [newCustomerID, name, points, email]);
       customerId = result.rows[0].customer_id;
     }
 
